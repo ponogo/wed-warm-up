e0750b5b2d324eada41d0f58b6f5f9a40b050e9a
Pavleg <pajabonki@gmail.com>
2020-07-22 17:35:19 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,10 @@
+num = input("give me a number ")
+
+num = int(num)
+
+if num > 0:
+  print("the number is positive")
+elif num < 0:
+  print("the number is negative")
+else:
+  print("equal to zero")
